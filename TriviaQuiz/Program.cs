0c8296ea54cf84158840a57fc777f78d1fc872a7
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-02-24 17:30:07 -0600

    Finished the GPA homework!!

---
+++ TriviaQuiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriviaQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
